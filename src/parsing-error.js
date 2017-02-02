a2d915a48e201a121fd550aa3390eaeb369717bb
Charles Pascoe <cpascoe95@gmail.com>
2017-02-02 20:32:57 +0000

    Add stack to ParsingError, for debugging

--- src/parsing-error.js
+++ src/parsing-error.js
@@ -1,5 +1,6 @@
 class ParsingError {
   constructor(start, length, message) {
+    this.stack = new Error().stack.split('\n').splice(2).join('\n');
     this.start = start;
     this.length = length;
     this.message = message;
