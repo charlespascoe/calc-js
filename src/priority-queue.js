0326099d0482b761cc086ff4173cf3070ec3c579
Charles Pascoe <cpascoe95@gmail.com>
2017-02-02 18:41:35 +0000

    Add primitive priority queue

---
+++ src/priority-queue.js
@@ -0,0 +1,26 @@
+class PriorityQueue {
+  constructor() {
+    this.queue = [];
+  }
+
+  add(priority, item) {
+    let elms = this.queue[priority];
+
+    if (!elms) {
+      elms = [];
+      this.queue[priority] = elms;
+    }
+
+    elms.push(item);
+  }
+
+  pop() {
+    for (let i = this.queue.length; i >= 0; i--) {
+      let elms = this.queue[i];
+
+      if (elms && elms.length > 0) return elms.splice(0, 1)[0];
+    }
+
+    return null;
+  }
+}
